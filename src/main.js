1ac8e5db5651caf3a011a5e54157e117bd10ad39
Nattapad Sa. <nattapad.sa@rmuti.ac.th>
2024-09-08 17:53:29 +0700

    add longdo map api init on main.js

--- src/main.js
+++ src/main.js
@@ -1,31 +1,36 @@
 /* eslint-disable import/order */
-import '@/@iconify/icons-bundle'
-import App from '@/App.vue'
-import vuetify from '@/plugins/vuetify'
-import { loadFonts } from '@/plugins/webfontloader'
-import router from '@/router'
-import '@core/scss/template/index.scss'
-import '@layouts/styles/index.scss'
-import '@styles/styles.scss'
-import { createPinia } from 'pinia'
-import { createApp } from 'vue'
-import VueSweetalert2 from 'vue-sweetalert2'
-import 'sweetalert2/dist/sweetalert2.min.css'
-import Vue3Lottie from 'vue3-lottie'
+import "@/@iconify/icons-bundle";
+import App from "@/App.vue";
+import vuetify from "@/plugins/vuetify";
+import { loadFonts } from "@/plugins/webfontloader";
+import router from "@/router";
+import "@core/scss/template/index.scss";
+import "@layouts/styles/index.scss";
+import "@styles/styles.scss";
+import { createPinia } from "pinia";
+import { createApp } from "vue";
+import VueSweetalert2 from "vue-sweetalert2";
+import "sweetalert2/dist/sweetalert2.min.css";
+import Vue3Lottie from "vue3-lottie";
+import LongdoMap from "longdo-map-vue";
 
-loadFonts()
-
+loadFonts();
 
 // Create vue app
-const app = createApp(App)
+const app = createApp(App);
 
-
+app
+  .use(LongdoMap, {
+    load: {
+      apiKey: import.meta.env.VITE_LONGDO_MAP_API_KEY,
+    },
+  })
 // Use plugins
-app.use(vuetify)
-app.use(createPinia())
-app.use(router)
-app.use(VueSweetalert2)
-app.use(Vue3Lottie)
+app.use(vuetify);
+app.use(createPinia());
+app.use(router);
+app.use(VueSweetalert2);
+app.use(Vue3Lottie);
 
 // Mount vue app
-app.mount('#app')
+app.mount("#app");
