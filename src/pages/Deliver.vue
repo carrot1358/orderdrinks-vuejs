4f62d80552b3f38c072fd4bad170c89fd693254d
Nattapad Sa. <nattapad.sa@rmuti.ac.th>
2024-09-27 14:17:53 +0700

    make deliver page connect to websocket. and update distance every time new location data sent from device

--- src/pages/Deliver.vue
+++ src/pages/Deliver.vue
@@ -3,11 +3,26 @@ import { ref, onMounted } from 'vue';
 import axios from 'axios';
 import { Order_ENDPOINTS } from '@/assets/config/api/api_endPoints';
 import DeliverList from '@/views/pages/deliver/DeliverList.vue';
+import { useWebSocket } from '@/assets/config/websocket/websocket'
+import { Websocket_URL_Frontend } from '@/assets/config/api/websocket_endPoints'
 import { VSkeletonLoader } from 'vuetify/components';
 import Swal from 'sweetalert2';
 
 const orders = ref([]);
 const loading = ref(false);
+
+const userInfo = ref(JSON.parse(localStorage.getItem('userinfo') || sessionStorage.getItem('userinfo') || '{}'))
+const { isConnected, lastMessage, error, send } = useWebSocket(`${Websocket_URL_Frontend}${userInfo.value.userId}`, {
+  reconnectInterval: 3000,
+  maxReconnectAttempts: 10,
+  heartbeatInterval: 20000,
+  heartbeatMessage: JSON.stringify({ type: 'ping' })
+})
+watch(lastMessage, (newMessage) => {
+  if (newMessage && newMessage.sendto === 'both' && newMessage.body.latitude && newMessage.body.longitude) {
+    updateDistance()
+  }
+})
 
 const fetchOrders = async () => {
   loading.value = true;
@@ -21,10 +36,25 @@ const fetchOrders = async () => {
     orders.value = response.data.data;
   } catch (error) {
     console.error('เกิดข้อผิดพลาดในการดึงข้อมูลคำสั่งซื้อ:', error);
+    orders.value = []
   } finally {
     loading.value = false;
   }
 };
+
+const updateDistance = async () => {
+  const jwtToken = localStorage.getItem('jwtToken') || sessionStorage.getItem('jwtToken');
+  try {
+    const response = await axios.get(Order_ENDPOINTS.getOderDelivery, {
+      headers: {
+        'Authorization': `Bearer ${jwtToken}`
+      }
+    });
+    orders.value = response.data.data;
+  } catch (error) {
+    console.error('เกิดข้อผิดพลาดในการอัพเดทระยะทาง:', error);
+  }
+}
 
 const prepareOrder = async () => {
   Swal.fire({
@@ -48,6 +78,7 @@ const prepareOrder = async () => {
             Swal.fire('สำเร็จ!', 'คำสั่งซื้อถูกเตรียมจัดส่งสำเร็จ', 'success', {
               timer: 1500
             });
+            fetchOrders();
           }
         }).catch((error) => {
           Swal.fire('ผิดพลาด!', error.response.data.message, 'error', {
@@ -76,6 +107,9 @@ onMounted(fetchOrders);
     </v-row>
     <v-card class="mt-5">
       <v-card-text>
+        <v-alert type="info" v-if="orders.length === 0">
+          ไม่พบคำสั่งซื้อที่ต้องการจัดส่ง
+        </v-alert>
         <DeliverList :orders="orders" :loading="loading" @refresh="fetchOrders" />
       </v-card-text>
     </v-card>
